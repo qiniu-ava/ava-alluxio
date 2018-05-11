c0356973e8a2e143c2085cb4a61c6098e65d0ca8
Bowen Xie <boxie5@126.com>
2018-05-11 15:07:15 +0800

    use default image

--- docker/zookeeper.entrypoint.sh
+++ docker/zookeeper.entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 echo "hello world"
 
-my_ip=`ifconfig | grep 'inet addr:172' | awk -F':' '{print $2}' | awk '{print $1}'`
+# my_ip=`ifconfig | grep 'inet addr:172' | awk -F':' '{print $2}' | awk '{print $1}'`
 
-echo "my_ip: $my_ip"
-my_id=`echo $my_ip | awk -F'.' '{print $4}'`
-echo "my_id: $my_id"
-export ZOO_MY_ID=$my_id
+# echo "my_ip: $my_ip"
+# my_id=`echo $my_ip | awk -F'.' '{print $4}'`
+# echo "my_id: $my_id"
+export ZOO_MY_ID=`hostname`
 
 # /docker-entrypoint.sh
 
