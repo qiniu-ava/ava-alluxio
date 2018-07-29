5ea1e2979264116763990f9c737e58c0d946563a
薛瑞 <xr94118@163.com>
2018-07-29 16:50:18 +0800

    update avio-server API

--- tools/golang/src/qiniu.com/app/avio-apiserver/services/jobs.go
+++ tools/golang/src/qiniu.com/app/avio-apiserver/services/jobs.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"qiniu.com/app/avio-apiserver/database"
 	"qiniu.com/app/avio-apiserver/typo"
@@ -20,36 +23,200 @@ func NewJobService() *JobService {
 func (j *JobService) CreateJob(c *gin.Context) {
 	job := &typo.JobSpec{}
 	if e := c.BindJSON(job); e != nil {
-		log.Warn("create job failed for invalid job spec, %v", e)
+		log.Warnf("create job failed for invalid job spec, %v", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
 		return
 	}
 
 	log.Debugf("trying to insert job with data: %v", job)
 
 	if e := job.Validate(); e != nil {
-		log.Warn("create job failed for invalid job spec, %v", e)
+		log.Warnf("create job failed for invalid job spec, %v", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
 		return
 	}
 
 	info, e := j.dao.InsertJob(job)
-	log.Warnf("insert job: %v", info)
 
 	if e != nil {
+		log.Warnf("insert job error: %v", e)
+		c.JSON(http.StatusNotImplemented, gin.H{
+			"err": e,
+		})
 		return
 	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"name": job.Name,
+		"item": info,
+	})
 }
 
 func (j *JobService) ListJobs(c *gin.Context) {
+
+	limit, e := strconv.Atoi(c.DefaultQuery("limit", "100"))
+	if e != nil {
+		log.Warnf("limit string2int error : ", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+
+	skip, e := strconv.Atoi(c.DefaultQuery("skip", "0"))
+	if e != nil {
+		log.Warnf("skip string2int error : ", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+	uid, e := strconv.Atoi(c.Query("uid"))
+	if e != nil {
+		log.Warnf("uid string2int error : ", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+
+	query := &database.ListJobQuery{limit, skip, uid}
+
+	result := &database.ListJobResult{}
+
+	if e := j.dao.ListJob(result, query); e != nil {
+		log.Warnf("list jobs error: %v", e)
+		c.JSON(http.StatusNotImplemented, gin.H{
+			"err": e,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"query": gin.H{
+			"limit": query.Limit,
+			"skip":  query.Skip,
+			"uid":   query.UID,
+		},
+		"total": result.Total,
+		"item":  result.Items,
+	})
+
 }
 
 func (j *JobService) GetJobInfo(c *gin.Context) {
-
+	jobName := c.Param("name")
+	headUID := c.GetHeader("X-UID")
+	if headUID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": "no uid in header",
+		})
+		return
+	}
+	uid, e := strconv.Atoi(headUID)
+	if e != nil {
+		log.Warnf("uid string2int error : ", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+	if e != nil {
+		log.Warnf("uid string2int error : ", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+	info, e := j.dao.GetJobInfo(jobName, uid)
+	if e != nil {
+		log.Warnf("get job info error: %v", e)
+		c.JSON(http.StatusNotImplemented, gin.H{
+			"err": e,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, info)
 }
 
 func (j *JobService) UpdateJobInfo(c *gin.Context) {
+	jobName := c.Param("name")
 
+	headUID := c.GetHeader("X-UID")
+	if headUID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": "no uid in header",
+		})
+		return
+	}
+	uid, e := strconv.Atoi(headUID)
+	if e != nil {
+		log.Warnf("uid string2int error : ", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+
+	jobInfo := &typo.JobInfo{}
+	if e := c.BindJSON(jobInfo); e != nil {
+		log.Warnf("update job failed for invalid job info, %v", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+
+	log.Debugf("trying to update job with data: %v", jobInfo)
+
+	if e := jobInfo.Validate(); e != nil {
+		log.Warnf("update job failed for invalid job info, %v", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+
+	updatedInfo, e := j.dao.UpdateJobInfo(jobName, uid, jobInfo)
+
+	if e != nil {
+		log.Warnf("update job error: %v", e)
+		c.JSON(http.StatusNotImplemented, gin.H{
+			"err": e,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, updatedInfo)
 }
 
 func (j *JobService) DeleteJobInfo(c *gin.Context) {
-
+	jobName := c.Param("name")
+	headUID := c.GetHeader("X-UID")
+	if headUID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": "no uid in header",
+		})
+		return
+	}
+	uid, e := strconv.Atoi(headUID)
+	if e != nil {
+		log.Warnf("uid string2int error : ", e)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": e,
+		})
+		return
+	}
+	if e := j.dao.DeleteJobInfo(jobName, uid); e != nil {
+		log.Warnf("delete job error: %v", e)
+		c.JSON(http.StatusNotImplemented, gin.H{
+			"err": e,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{})
 }
--- tools/golang/src/qiniu.com/app/avio-apiserver/typo/job.go
+++ tools/golang/src/qiniu.com/app/avio-apiserver/typo/job.go
@@ -1,8 +1,10 @@
 package typo
 
 import (
+	"net/url"
 	"time"
 
+	"github.com/pkg/errors"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -25,22 +27,22 @@ const (
 )
 
 type JobParams struct {
-	AlluxioURI   string `json:"alluxioUri"`
-	Depth        int    `json:"depth"`
-	FromFileList bool   `json:"fromFileList"`
+	AlluxioURI   string `json:"alluxioUri" bson:"alluxioUri"`
+	Depth        int    `json:"depth" bson:"depth"`
+	FromFileList bool   `json:"fromFileList" bson:"fromFileList"`
 }
 
 type JobSpec struct {
-	Name   string    `json:"name"`
-	UID    int       `json:"uid"`
-	Type   JobType   `json:"type"`
-	Params JobParams `json:"params"`
+	Name   string    `json:"name" bson:"name"`
+	UID    int       `json:"uid" bson:"uid"`
+	Type   JobType   `json:"type" bson:"type"`
+	Params JobParams `json:"params" bson:"params"`
 }
 
 type JobInfo struct {
 	ID         bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	JobSpec    `bson:",inline"`
-	Status     JobStatus  `json:"status"`
+	JobSpec    `bson:",inline" bson:"jobSpec"`
+	Status     JobStatus  `json:"status" bson:"status"`
 	CreateTime *time.Time `json:"createTime,omitempty" bson:"createTime,omitempty"`
 	UpdateTime *time.Time `json:"updateTime,omitempty" bson:"updateTime,omitempty"`
 	FinishTime *time.Time `json:"finishTime,omitempty" bson:"finishTime,omitempty"`
@@ -48,16 +50,36 @@ type JobInfo struct {
 }
 
 func (t *JobType) Validate() error {
-	// TODO implement me
+	if *t != ListJobType && *t != PreloadJobType && *t != SaveJobType && *t != StatJobType {
+		return errors.Errorf("illegal jobType")
+	}
 	return nil
 }
 
 func (p *JobParams) Validate() error {
-	// TODO implement me
+	if _, e := url.Parse(p.AlluxioURI); e != nil {
+		return e
+	}
 	return nil
 }
 
 func (s *JobSpec) Validate() error {
-	// TODO implement me
+	if e := s.Type.Validate(); e != nil {
+		return e
+	}
+	if e := s.Params.Validate(); e != nil {
+		return e
+	}
+	return nil
+}
+
+func (i *JobInfo) Validate() error {
+	if e := i.JobSpec.Validate(); e != nil {
+		return e
+	}
+	if i.Status != CreatedJobStatus && i.Status != RunningJobStatus &&
+		i.Status != SuccessJobStatus && i.Status != FailedJobStatus {
+		return errors.Errorf("illegal jobType")
+	}
 	return nil
 }
