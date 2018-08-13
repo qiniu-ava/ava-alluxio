b7a74d065aaf18d8641a9ed4d7b490aa248eca07
Bowen Xie <boxie5@126.com>
2018-08-13 18:35:37 +0800

    workaround zookeeper error, use standalone mode

--- deploy/docker/alluxio.zookeeper.sh
+++ deploy/docker/alluxio.zookeeper.sh
@@ -4,14 +4,13 @@ set -ex
 
 source /root/env
 
-if [ $ZOO_SERVERS = "" ]; then
+if [ "$ZOO_SERVERS" = "" ]; then
   echo "ZOO_SERVERS not set"
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-myip=`ifconfig | grep 'inet addr:192.168.212' | awk -F':' '{print $2}' | awk '{print $1}'`
-myid=`echo $myip | awk -F'.' '{print $4}'`
+myip=`ifconfig | grep 'inet addr:192.168.213' | awk -F':' '{print $2}' | awk '{print $1}'`
 zoo_servers=`echo $ZOO_SERVERS | sed "s|\$myip|0.0.0.0|"`
 
 mkdir -m 777 -p /alluxio-share/zookeeper/$myip/config /alluxio-share/zookeeper/$myip/data /alluxio-share/zookeeper/$myip/log
@@ -24,10 +23,14 @@ docker rm -f alluxio-zk
 docker run -d \
   --network host \
   --name alluxio-zk \
-  -e ZOO_MY_ID=${myid} \
+  -e ZOO_MY_ID=${ZK_MY_ID} \
+  -e ZOO_MAX_CLIENT_CNXNS=3600 \
   -e ZOO_SERVERS=${zoo_servers} \
   -v /alluxio-share/zookeeper/$myip/config:/conf \
   -v /alluxio-share/zookeeper/$myip/data:/data \
   -v /alluxio-share/zookeeper/$myip/log:/datalog \
+  -p 2181:2181 \
+  -p 2888:2888 \
+  -p 3888:3888 \
   --restart=always \
   zookeeper:3.4
