9be8ef909c5fd96bad5c7631420721892c10cfa7
Bowen Xie <boxie5@126.com>
2018-09-10 15:10:53 +0800

    ATLAB-8094 add alluxio regression test

--- tools/scripts/build-alluxio.sh
+++ tools/scripts/build-alluxio.sh
@@ -57,6 +57,8 @@ done
 
 mkdir -p .tmp/alluxio .tmp/kodo .tmp/temp/kodo
 
+alluxio_version=$(grep -m1 "<version>" ${local_alluxio}/pom.xml | awk -F'>|<' '{print $3}')
+
 ########################################################################### 
 # build alluxio tarball and extract
 ###########################################################################
@@ -66,12 +68,12 @@ if [ $build_tarball != "false" ]; then
   rm -rf .tmp/alluxio/* .tmp/temp/kodo/* && \
     cd .tmp/alluxio && \
     ${local_alluxio}/dev/scripts/generate-tarballs single && \
-    tar xf alluxio-1.7.2-SNAPSHOT.tar.gz && \
-    rm alluxio-1.7.2-SNAPSHOT.tar.gz && \
+    tar xf alluxio-"${alluxio_version}".tar.gz && \
+    rm alluxio-"${alluxio_version}".tar.gz && \
     cd .. && \
-    cp alluxio/alluxio-1.7.2-SNAPSHOT/lib/alluxio-underfs-oss-1.7.2-SNAPSHOT.jar ./temp/ && \
+    cp alluxio/alluxio-"${alluxio_version}"/lib/alluxio-underfs-oss-1.8.1-SNAPSHOT.jar ./temp/ && \
     cd temp/kodo/ && \
-    jar xf ../alluxio-underfs-oss-1.7.2-SNAPSHOT.jar
+    jar xf ../alluxio-underfs-oss-"${alluxio_version}".jar
   cd $DIR/../.. && \
     echo -e "\n\n\n"
 else
@@ -88,11 +90,11 @@ if [ $build_kodo != "false" ]; then
   mvn -DskipTests -Dlicense.skip=true compile install
   rm -rf $DIR/../../.tmp/temp/kodo/com && cp -r target/classes/com $DIR/../../.tmp/temp/kodo/com
   cd $DIR/../../.tmp/temp/kodo && \
-    rm -f alluxio-underfs-oss-1.7.2-SNAPSHOT.jar && \
-    jar -cf alluxio-underfs-oss-1.7.2-SNAPSHOT.jar . && \
-    mv ./alluxio-underfs-oss-1.7.2-SNAPSHOT.jar $DIR/../../.tmp/alluxio/alluxio-1.7.2-SNAPSHOT/lib/ && \
+    rm -f alluxio-underfs-oss-"${alluxio_version}".jar && \
+    jar -cf alluxio-underfs-oss-"${alluxio_version}".jar . && \
+    mv ./alluxio-underfs-oss-"${alluxio_version}".jar $DIR/../../.tmp/alluxio/alluxio-1.8.1-SNAPSHOT/lib/ && \
     cd $DIR/../.. && \
-    cp $DIR/docker-image/kodo-libs/* .tmp/alluxio/alluxio-1.7.2-SNAPSHOT/lib/ && \
+    cp $DIR/docker-image/kodo-libs/* .tmp/alluxio/alluxio-"${alluxio_version}"/lib/ && \
     echo -e "\n\n\n"
 else
   echo -e "skip building kodo sdk\n\n\n"
@@ -108,7 +110,7 @@ if [ $build_image != "false" ]; then
   cp $DIR/docker-image/entrypoint.sh .tmp/alluxio/
   cd $local_alluxio && alluxio_hash=`git rev-parse --short=7 HEAD` && cd -
   cd $local_kodo && kodo_hash=`git rev-parse --short=7 HEAD` && cd -
-  docker build -t alluxio:$alluxio_hash-$kodo_hash -f $DIR/docker-image/Dockerfile.alluxio .tmp/alluxio
+  docker build -t alluxio:$alluxio_hash-$kodo_hash --build-arg ALLUXIO_VERSION="${alluxio_version}" -f $DIR/docker-image/Dockerfile.alluxio .tmp/alluxio
   if [ $push_image != "false" ]; then
     docker tag alluxio:$alluxio_hash-$kodo_hash reg-xs.qiniu.io/atlab/alluxio:$alluxio_hash-$kodo_hash
     docker push reg-xs.qiniu.io/atlab/alluxio:$alluxio_hash-$kodo_hash
