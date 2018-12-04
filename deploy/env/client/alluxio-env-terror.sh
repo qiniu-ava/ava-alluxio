7a3bdc09cbc0a9d96ff59eeada0654f82edadeac
nnrg123 <13601648153@163.com>
2018-12-04 17:19:39 +0800

    add a terror group

---
+++ deploy/env/client/alluxio-env-terror.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 此文件需要在 kubernetes 节点部署 client 时使用
+
+export ALLUXIO_JAVA_OPTS="-Xmx16G"
+export QINIU_WRITER_HOSTS="192.168.217.125:24899,192.168.217.127:24899"
+export QINIU_READER_EX_HOSTS="192.168.217.125:24899,192.168.217.127:24899"
