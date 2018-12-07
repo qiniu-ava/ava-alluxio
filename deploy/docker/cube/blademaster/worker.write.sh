cfdd1284346b351ff5bc4b7f2310665addb3ca71
nnrg123 <13601648153@163.com>
2018-12-07 14:50:35 +0800

    add group : terror, blademaster, general-reg, ocr

--- deploy/docker/cube/blademaster/worker.write.sh
+++ deploy/docker/cube/blademaster/worker.write.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # worker node list:
-# jq65 jq66
+# jq66 jq67
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export GROUP=terror
-export NODE_LIST="jq65 jq66"
+export GROUP=blademaster
+export NODE_LIST="jq66 jq67"
 
 ${DIR}/../template/worker.write.sh "$@"
--- deploy/docker/cube/terror/master.sh
+++ deploy/docker/cube/terror/master.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # master node list:
-# jq67
+# jq70
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GROUP=terror
-export NODE_LIST="jq67"
+export NODE_LIST="jq70"
 
 ${DIR}/../template/master.sh "$@"
