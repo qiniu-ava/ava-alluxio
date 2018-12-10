9ffa1c8d5e6b471fd419e2208d6775e05154ce65
Bowen Xie <boxie5@126.com>
2018-12-10 14:20:23 +0800

    update alluxio and hack scripts

--- hack/alluxio/worker.write.sh
+++ hack/alluxio/worker.write.sh
@@ -7,36 +7,38 @@ if [[ ! -f "$DIR/env" ]]; then
   exit 1
 fi
 
+name=alluxio-worker-write
+
 # shellcheck disable=SC1090
 source "$DIR"/env
+
+
 myip=$(ifconfig | grep -E -A6 "^en0:|^bond0" | grep -E "^\\s+inet " | grep -o -E  '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}' | head -n 1)
-docker rm -f alluxio-worker
+docker rm -f "${name}"
 docker run -d \
-  --name alluxio-worker \
+  --name "${name}" \
   --hostname "${myip}" \
   -e ALLUXIO_UNDERFS_ADDRESS=/underStorage \
   -e KODO_IO_ORIGHOST="${KODO_IO_ORIGHOST}" \
   -e KODO_UP_ORIGHOST="${KODO_UP_ORIGHOST}" \
-  -e ALLUXIO_WORKER_MEMORY_SIZE="${ALLUXIO_WORKER_MEMORY_SIZE}" \
   -e ALLUXIO_WORKER_TIEREDSTORE_LEVELS=1 \
   -e ALLUXIO_WORKER_TIEREDSTORE_LEVEL0_ALIAS=SSD \
-  -e ALLUXIO_WORKER_TIEREDSTORE_LEVEL0_DIRS_PATH=/opt/cachedisk-read \
+  -e ALLUXIO_WORKER_TIEREDSTORE_LEVEL0_DIRS_PATH=/opt/cachedisk-write \
   -e ALLUXIO_WORKER_TIEREDSTORE_LEVEL0_DIRS_QUOTA=10GB \
   -e ALLUXIO_WORKER_TIEREDSTORE_LEVEL0_WATERMARK_HIGH_RATIO=0.8 \
   -e ALLUXIO_WORKER_TIEREDSTORE_LEVEL0_WATERMARK_LOW_RATIO=0.7 \
   -e ALLUXIO_WORKER_TIEREDSTORE_RESERVER_ENABLED=true \
   -e ALLUXIO_WORKER_TIEREDSTORE_RESERVER_INTERVAL=10000 \
   -e ALLUXIO_CLASSPATH=/opt/alluxio/lib/gson-2.2.4.jar:/opt/alluxio/lib/qiniu-java-sdk-7.2.11.jar:/opt/alluxio/lib/okhttp-3.10.0.jar:/opt/alluxio/lib/okio-1.14.0.jar:/opt/alluxio/lib/jackson-databind-2.9.5.jar:/opt/alluxio/lib/jackson-core-2.9.5.jar:/opt/alluxio/lib/jackson-annotations-2.9.5.jar \
-  -e ALLUXIO_WORKER_PORT="${ALLUXIO_WORKER_PORT}" \
-  -e ALLUXIO_WORKER_DATA_PORT="${ALLUXIO_WORKER_DATA_PORT}" \
-  -e ALLUXIO_WORKER_WEB_PORT="${ALLUXIO_WORKER_WEB_PORT}" \
+  -e ALLUXIO_WORKER_PORT="${ALLUXIO_WRITE_WORKER_PORT}" \
+  -e ALLUXIO_WORKER_DATA_PORT="${ALLUXIO_WRITE_WORKER_DATA_PORT}" \
+  -e ALLUXIO_WORKER_WEB_PORT="${ALLUXIO_WRITE_WORKER_WEB_PORT}" \
   -e ALLUXIO_MASTER_HOSTNAME="${myip}" \
-  -e ALLUXIO_WORKER_HOSTNAME="${myip}" \
-  -e ALLUXIO_WORKER_ROLE=READ \
-  -p "${ALLUXIO_WORKER_PORT}":"${ALLUXIO_WORKER_PORT}" \
-  -p "${ALLUXIO_WORKER_DATA_PORT}":"${ALLUXIO_WORKER_DATA_PORT}" \
-  -p "${ALLUXIO_WORKER_WEB_PORT}":"${ALLUXIO_WORKER_WEB_PORT}" \
-  -v "${ALLUXIO_WORKER_SSD_DISK}":/opt/cachedisk-read \
+  -e ALLUXIO_WORKER_ROLE=WRITE \
+  -p "${ALLUXIO_WRITE_WORKER_PORT}":"${ALLUXIO_WRITE_WORKER_PORT}" \
+  -p "${ALLUXIO_WRITE_WORKER_DATA_PORT}":"${ALLUXIO_WRITE_WORKER_DATA_PORT}" \
+  -p "${ALLUXIO_WRITE_WORKER_WEB_PORT}":"${ALLUXIO_WRITE_WORKER_WEB_PORT}" \
+  -v "${ALLUXIO_WRITE_WORKER_SSD_DISK}":/opt/cachedisk-write \
   --restart=always \
   alluxio \
   worker --no-format
