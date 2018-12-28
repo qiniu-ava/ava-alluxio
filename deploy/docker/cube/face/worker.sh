a575e3ae104b8cb3f9fd8107149c1e2d6ac278fe
nnrg123 <13601648153@163.com>
2018-12-28 16:18:22 +0800

    update group face

--- deploy/docker/cube/face/worker.sh
+++ deploy/docker/cube/face/worker.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # worker node list:
-# jq40 jq41 jq42
+# jq20 jq21 jq40 jq41 jq42
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GROUP=face
-export NODE_LIST="jq40 jq41 jq42"
+export NODE_LIST="jq20 jq21 jq40 jq41 jq42"
 
 ${DIR}/../template/worker.sh "$@"
--- deploy/docker/cube/face/worker.write.sh
+++ deploy/docker/cube/face/worker.write.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # worker node list:
-# jq40 jq41
+# jq40 jq41 jq42
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GROUP=face
-export NODE_LIST="jq40 jq41"
+export NODE_LIST="jq40 jq41 jq42"
 
 ${DIR}/../template/worker.write.sh "$@"
