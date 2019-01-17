dbb03f845e6a8e72b9c4ce3662c91662c4dae2d9
nnrg123 <13601648153@163.com>
2019-01-17 14:12:22 +0800

    change workers and SSD of some groups

--- deploy/docker/cube/blademaster/worker.sh
+++ deploy/docker/cube/blademaster/worker.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # worker node list:
-# jq65 jq66 jq67 jq68 jq69
+# jq65 jq66
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GROUP=blademaster
-export NODE_LIST="jq65 jq66 jq67 jq68 jq69"
+export NODE_LIST="jq65 jq66"
 
 ${DIR}/../template/worker.sh "$@"
--- deploy/docker/cube/video/worker.sh
+++ deploy/docker/cube/video/worker.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # worker node list:
-# jq40 jq41 jq42 jq56 jq57
+# jq40 jq41 jq42 jq56 jq57 jq67 jq68 jq69
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GROUP=video
-export NODE_LIST="jq40 jq41 jq42 jq56 jq57"
+export NODE_LIST="jq40 jq41 jq42 jq56 jq57 jq67 jq68 jq69"
 
 ${DIR}/../template/worker.sh "$@"
