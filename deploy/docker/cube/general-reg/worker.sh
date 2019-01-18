5a1f3536ea8fb770f2de01b87944ae11015db641
nnrg123 <13601648153@163.com>
2019-01-18 10:20:48 +0800

    change workers and SSD of some groups

--- deploy/docker/cube/general-reg/worker.sh
+++ deploy/docker/cube/general-reg/worker.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # worker node list:
-# jq65 jq66 jq67 jq68 jq69 jq70 jq71 jq73
+# jq65 jq66 jq67 jq68 jq69 jq70 jq71 jq73 jq19 jq21
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GROUP=general-reg
-export NODE_LIST="jq65 jq66 jq67 jq68 jq69 jq70 jq71 jq73"
+export NODE_LIST="jq65 jq66 jq67 jq68 jq69 jq70 jq71 jq73 jq19 jq21"
 
 ${DIR}/../template/worker.sh "$@"
--- deploy/docker/cube/ocr/worker.sh
+++ deploy/docker/cube/ocr/worker.sh
@@ -2,12 +2,12 @@
 
 ######################################################################
 # worker node list:
-# jq68 jq69
+# jq68 jq69 jq40 jq41
 ######################################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export GROUP=ocr
-export NODE_LIST="jq68 jq69"
+export NODE_LIST="jq68 jq69 jq40 jq41"
 
 ${DIR}/../template/worker.sh "$@"
